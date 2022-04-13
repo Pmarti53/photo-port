847229093d3a5abbfaf22712c0ef6620a4e79bbd
peter <ptmartinezsound@gmail.com>
2022-04-12 19:16:26 -0500

    created nav component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import About from './components/About';
+import Nav from './components/Nav';
+
 import './App.css';
 
 function App() {
   return (
     <div>
       <main>
+        <Nav></Nav>
         <About></About>
       </main>
 
