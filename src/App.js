f0f4240e63ce22e075a45fecbbfe2d2c70f860a9
peter <ptmartinezsound@gmail.com>
2022-04-14 10:04:42 -0500

    added gallery compnent

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 import './App.css';
 
 function App() {
   return (
     <div>
+      <Nav></Nav>
       <main>
-        <Nav></Nav>
+        <Gallery></Gallery>
         <About></About>
       </main>
-
     </div>
   );
 }
+
+
 
 export default App;
